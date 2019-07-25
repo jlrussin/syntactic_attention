6a884a0535813eec100308eb7e26f79d7262862d
jlrussin <jlrussin@gmail.com>
2019-07-25 15:05:07 -0600

    Added vocab generation function

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from data import ScanDataset
+import json
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -5,3 +8,9 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def generate_vocab_json(data_file,vocab_out_json):
+    dataset = ScanDataset(data_file)
+    vocab = dataset.vocab
+    with open(vocab_out_json,'w') as f:
+        json.dump(vocab,f)
