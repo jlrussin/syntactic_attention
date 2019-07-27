a2ab318f4b53c639a73c8190b9fa8741b0024f5c
jlrussin <jlrussin@gmail.com>
2019-07-27 10:24:05 -0600

    Scripts for ecortex

---
+++ scripts/ecortex/train_semmlp_eng_MT.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#SBATCH -p local
+#SBATCH -A ecortex
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -c 4
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+python train_test.py \
+--dataset MT \
+--flip True \
+--train_data_file data/MT/train.daxy \
+--test_data_file data/MT/test.daxy \
+--load_vocab_json vocab_eng_MT_daxiste.json \
+--sem_mlp True \
+--learning_rate 0.001 \
+--results_dir train_results \
+--out_data_file train_defaults_eng_MT.json \
+--checkpoint_path ../model_weights/semmlp_eng_MT.pt
