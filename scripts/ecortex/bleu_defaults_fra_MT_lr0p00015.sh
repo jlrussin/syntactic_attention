0cda42ba709ca42dad167f191a39bdb23af44a51
jlrussin <jlrussin@gmail.com>
2019-07-28 11:13:22 -0600

    scripts

---
+++ scripts/ecortex/bleu_defaults_fra_MT_lr0p00015.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#SBATCH -p localBackground
+#SBATCH -A ecortex
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+#SBATCH -c 4
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+python evaluate_bleu.py \
+--dataset MT \
+--flip True \
+--test_data_file data/MT/train.daxy \
+--load_vocab_json vocab_eng_MT_daxiste.json \
+--load_weights_from ../model_weights/defaults_fra_MT_lr0p00015.pt
