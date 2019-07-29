077494e555915425901eccce312a02884ff5ca94
jlrussin <jlrussin@gmail.com>
2019-07-29 15:31:14 -0600

    new scripts

---
+++ scripts/ecortex/bleu_defaults_eng_MT_test_lr0p00015.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#SBATCH -p localBackground
+#SBATCH -A ecortex
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+#SBATCH -c 4
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+python evaluate_bleu.py \
+--dataset MT \
+--flip True \
+--train_data_file data/MT/train \
+--val_data_file data/MT/test_no_unk.txt \
+--test_data_file data/MT/test \
+--load_vocab_json vocab_eng_MT_all.json \
+--load_weights_from ../model_weights/defaults_eng_MT_test_lr0p00015.pt
