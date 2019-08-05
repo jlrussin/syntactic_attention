f8219cafb75ca435f6508a441ec402498682e542
jlrussin <jlrussin@gmail.com>
2019-08-05 10:05:56 -0700

    unused scripts - already submitted results

---
+++ scripts/ecortex/bleu_synact_eng_MT_test_lr0p00015_rep5.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH -p localBackground
+#SBATCH -A ecortex
+#SBATCH --mem=10G
+#SBATCH --time=6:00:00
+#SBATCH -c 4
+
+export HOME=`getent passwd $USER | cut -d':' -f6`
+export PYTHONUNBUFFERED=1
+echo Running on $HOSTNAME
+
+source /usr/local/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch1.0
+
+python evaluate_bleu.py \
+--dataset MT \
+--flip True \
+--train_data_file data/MT/train \
+--val_data_file data/MT/test_no_unk.txt \
+--test_data_file data/MT/test \
+--load_vocab_json vocab_eng_MT_all.json \
+--syn_act True \
+--load_weights_from ../model_weights/synact_eng_MT_test_lr0p00015_rep5.pt
